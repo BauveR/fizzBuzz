a8a6e8cf8250e9f36f9c61d33b446d6e673b8777
BauveR <rick.bauve@gmail.com>
2025-06-05 09:48:21 +0200

    message structure of fizzbuzz initiat structure try catch

--- src/scripts/core/fizzBuzz.js
+++ src/scripts/core/fizzBuzz.js
@@ -1,5 +1,29 @@
 
 
 export const fizzBuzz = (userData) => {
-    
+
+    let response = {
+        status: "ok",
+        message: "",
+        data:{
+            input: userData,
+            ouput: "",
+        }
+    };
+
+    try { 
+        if(
+             userData !== "number" || !Number.isIntegrer(userData) || userData < 0
+        ){
+            throw new Error ("only positive numbers allowed")
+        }
+
+
+    }catch{
+
+
+    }
+
+
+
 }
