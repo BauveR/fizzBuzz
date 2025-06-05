14cf285ee21ef8f0d27687c65ad853fb8f92ecda
BauveR <rick.bauve@gmail.com>
2025-06-05 10:57:30 +0200

    creating results to save the data accepted and rejected

--- src/scripts/core/fizzBuzz.js
+++ src/scripts/core/fizzBuzz.js
@@ -15,19 +15,29 @@ export const fizzBuzz = (userData) => {
         if(
              userData !== "number" || !Number.isIntegrer(userData) || userData < 0
         ){
-            throw new Error ("only positive numbers allowed")
+            throw new Error ("only positive numbers allowed");
 
         } if (userData % 5 === 0 && userData % 3 === 0 ){
-            response.message = "Number dubidible by 3 & 5"
-        } else if 
+            response.message = "Number duvidible by 3 & 5";
+            response.data.ouput = "FizzBuzz";
+        } else if (userData % 3 === 0){
+            response.message = "Number dividible by 3";
+            response.data.ouput = "Fizz";
+        } else if (userData % 5 === 0){
+            response.message = "Number dividible by 5";
+            response.data.ouput = "Buzz";
+        } else {
+            response.message = "Number not dividible by 5 or 3";
+            response.data.ouput = userData;
+        }
 
+        return response;
 
-    }catch(error();
-    ){
-
-
+    }catch(error){
+        console.error (`error on input data ${error}`)
+            response.status = "Invalid Request"
+            response.message = "Unprocessable Input"
+            response.data.ouput = "Invalid input"
+            return response;
     }
-
-
-
 }
---
+++ src/scripts/data/results.js
@@ -0,0 +1,2 @@
+export const responsesOk = [];
+export const responsesRejected = [];
