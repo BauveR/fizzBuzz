55cec8afd1541e03e1f9095d59079da1bd2b4b32
BauveR <rick.bauve@gmail.com>
2025-06-05 10:46:05 +0200

    function fizz buzz

--- src/scripts/core/fizzBuzz.js
+++ src/scripts/core/fizzBuzz.js
@@ -16,10 +16,14 @@ export const fizzBuzz = (userData) => {
              userData !== "number" || !Number.isIntegrer(userData) || userData < 0
         ){
             throw new Error ("only positive numbers allowed")
-        }
+
+        } if (userData % 5 === 0 && userData % 3 === 0 ){
+            response.message = "Number dubidible by 3 & 5"
+        } else if 
 
 
-    }catch{
+    }catch(error();
+    ){
 
 
     }
